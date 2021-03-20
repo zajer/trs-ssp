29d9c54eb5ac1e2fede9a158b5ea14c1d4ebc1d9
zajer <pikus3@list.pl>
2021-03-20 17:25:48 +0100

    Extended Square_matrix module. It can not make a matrix based on array of its elements.

--- src/square_matrix.ml
+++ src/square_matrix.ml
@@ -8,3 +8,10 @@ let _get_element_in_ith_row_jth_column matrix ~i ~j =
 let column matrix column =
     let result = Array.init matrix.length  (fun i -> _get_element_in_ith_row_jth_column ~i:i ~j:column matrix ) in
         result
+let make elts = 
+    let number_of_rows = Array.length elts in
+    let are_elts_valid = Array.fold_left (fun res row -> (Array.length row = number_of_rows) && res) true elts in
+    if are_elts_valid then
+        {elements=elts;length=number_of_rows}
+    else
+        raise (Invalid_argument "Provided elements do not create square matrix")
