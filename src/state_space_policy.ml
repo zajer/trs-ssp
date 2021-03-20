501b0b215b28736ec4996deebbcdfe4e0fd5f900
zajer <pikus3@list.pl>
2021-03-20 06:48:51 +0100

    Modified 'state_space_policy' module interface.

--- src/state_space_policy.ml
+++ src/state_space_policy.ml
@@ -2,8 +2,8 @@
 module type S = sig
     type situation
     type state_trans_fun 
-    type situations_in_state = Some of situation Seq.t | Not_reachable
-    type courses_between_situations = Some of state_trans_fun Seq.t | No_transitions
+    type situations_in_state = Situations of situation Seq.t | Not_reachable
+    type courses_between_situations = Courses of state_trans_fun Seq.t | No_transitions
     (*
     type system_situation_matrix = situation Seq.t array
     type system_transformation_matrix = state_trans_fun Seq.t array array
@@ -16,23 +16,24 @@ module type S = sig
     *)
     val convolute : situations_in_state -> courses_between_situations -> situations_in_state
     val multiply : system_situation_matrix -> system_transformation_matrix -> system_situation_matrix
+    val init_situation_in_state : situation -> situations_in_state
 end
 
 module Make ( S : State_space.SS) =
 struct
     type situation = S.situation 
     type state_trans_fun = S.state_trans_fun
-    type situations_in_state = Some of situation Seq.t | Not_reachable
-    type courses_between_situations = Some of state_trans_fun Seq.t | No_transitions
+    type situations_in_state = Situations of situation Seq.t | Not_reachable
+    type courses_between_situations = Courses of state_trans_fun Seq.t | No_transitions
     type system_situation_matrix = situations_in_state array
     type system_transformation_matrix = courses_between_situations Square_matrix.t
 
     let convolute situations  trans_funs = 
         match situations,trans_funs with
         | Not_reachable,No_transitions -> Not_reachable
-        | Some _, No_transitions -> Not_reachable
-        | Not_reachable, Some _ -> Not_reachable
-        | Some sits, Some trans_funs -> 
+        | Situations _, No_transitions -> Not_reachable
+        | Not_reachable, Courses _ -> Not_reachable
+        | Situations sits, Courses trans_funs -> 
             let result = Seq.flat_map 
                 (
                     fun sit -> 
@@ -44,13 +45,13 @@ struct
                 )
                 sits
             in
-            Some result
+            Situations result
     let _merge_situations_at_state sits1 sits2 = 
         match sits1, sits2 with
         | Not_reachable, Not_reachable -> Not_reachable
-        | Some s1, Not_reachable -> Some s1
-        | Not_reachable, Some s2 -> Some s2
-        | Some s1, Some s2 -> Some (Seq.append s1 s2)
+        | Situations s1, Not_reachable -> Situations s1
+        | Not_reachable, Situations s2 -> Situations s2
+        | Situations s1, Situations s2 -> Situations (Seq.append s1 s2)
     let multiply situations_mx trans_mx =
         let result = Array.mapi 
         (
@@ -73,5 +74,8 @@ struct
         (Array.make trans_mx.length [])
         in
             result
-
+    let init_situation_in_state (sit:situation) =
+        let res =  Seq.cons sit (Seq.empty)
+        in
+            (Situations res)
 end
--- src/state_space_policy.mli
+++ src/state_space_policy.mli
@@ -1,13 +1,14 @@
 module type S = sig
     type situation
     type state_trans_fun 
-    type situations_in_state = Some of situation Seq.t | Not_reachable
-    type courses_between_situations = Some of state_trans_fun Seq.t | No_transitions
+    type situations_in_state = Situations of situation Seq.t | Not_reachable
+    type courses_between_situations = Courses of state_trans_fun Seq.t | No_transitions
     type system_situation_matrix = situations_in_state array
     type system_transformation_matrix = courses_between_situations Square_matrix.t
     
     val convolute : situations_in_state -> courses_between_situations -> situations_in_state
     val multiply : system_situation_matrix -> system_transformation_matrix -> system_situation_matrix
+    val init_situation_in_state : situation -> situations_in_state
 end
 
 module Make ( S : State_space.SS) : S with type situation = S.situation and type state_trans_fun = S.state_trans_fun
