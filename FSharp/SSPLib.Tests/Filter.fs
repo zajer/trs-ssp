4359d2f888e20f87cd53e67098798d38060073fe
zajer <pikus3@list.pl>
2021-05-27 11:52:12 +0200

    Wrapped the projects into solution. Added a test project.

--- FSharp/SSPLib.Tests/Filter.fs
+++ FSharp/SSPLib.Tests/Filter.fs
@@ -7,7 +7,15 @@ type FilterTestClass () =
 
     [<TestMethod>]
     member this.FilterMostEngagingTest1 () =
-        let situation = SSPLib.State.ofArray [|(1,2);(2,2);(3,2)|]
-        let minEngagement = 3
-        //let result = SSPLib.Filter.filterMostEngaging minEngagement situation
-        Assert.IsTrue(true);
+        let sat = SSPLib.State.ofArray [|(1,2);(2,2);(3,2)|]
+        let situation = SSPLib.StateSpace.initSituation sat
+        let minEngagement = 3.0
+        let result = SSPLib.Filter.filterMostEngaging minEngagement situation
+        Assert.IsTrue(result);
+    [<TestMethod>]
+    member this.FilterMostEngagingTest2 () =
+        let sat = SSPLib.State.ofArray [|(1,2);(2,2);(3,2)|]
+        let situation = SSPLib.StateSpace.initSituation sat
+        let minEngagement = 2.0
+        let result = SSPLib.Filter.filterMostEngaging minEngagement situation
+        Assert.IsTrue(result);
